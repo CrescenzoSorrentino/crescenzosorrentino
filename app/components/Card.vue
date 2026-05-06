33c1b2dad7cf68fb6d3a3ba2fb83e3affe521444
CrescenzoSorrentino <crescenzo.sorrentino@icloud.com>
2026-05-06 15:27:47 +0200

    fix(card): use CSS sizes syntax for more accurate responsive images

--- app/components/Card.vue
+++ app/components/Card.vue
@@ -19,7 +19,7 @@ defineProps({
         :src="image"
         :alt="title"
         class="preview__img"
-        sizes="sm:100vw md:50vw lg:33vw"
+        sizes="(max-width: 639px) 100vw, (max-width: 1023px) 50vw, 33vw"
         format="webp"
         quality="80"
         loading="lazy"
