3f0b7cafd81d8cb82ad4cdd79acb38a757ebc639
CrescenzoSorrentino <crescenzo.sorrentino@icloud.com>
2026-05-23 23:58:21 +0200

    feat: make avatar size responsive on mobile using CSS custom properties

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -71,7 +71,7 @@ useSeoMeta({
           <BaseAvatar
             src="/crescenzosorrentino.png"
             alt="Crescenzo Sorrentino"
-            :size="280"
+            :size="320"
             v-motion
             :initial="{ opacity: 0, x: -32 }"
             :visible-once="{ opacity: 1, x: 0, transition: { duration: 600 } }"
